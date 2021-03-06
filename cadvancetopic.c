3f2708d457984becd7a96dbf8b92f32be31f218b
IamMoizAnsari <moizmit02@gmail.com>
2021-03-06 12:02:55 +0530

    all question list files

---
+++ cadvancetopic.c
@@ -0,0 +1,16 @@
+1 pointer
+2 enums
+3 function parameter passing
+4 array with pointer
+5 array to pass a function via pointer
+6 structure
+7 structure with pointer
+8 structure to pass a function by using pointer
+9 file hangling 
+10 file handling with structure
+11 file hangling with structure and pointer
+12 malloc and calloc and realloc
+13 dynamic memory allocation
+14 string all function
+15 array all function
+16 file handling all function
---
+++ dsContent.c
@@ -0,0 +1,98 @@
+1 basic of datastructure
+2 array
+
+3 linked list , singlly linked list , doublly linked list , circular doublly linked list
+4 array vs linkList
+5 linked list implementation
+6 add a new node at start , end or specific position in linkedlist
+7 delete a node from start , end or specific position in linkedlist
+find length of linked list 
+8 reverse linkedlist
+5 doubly linked list implementation
+  add a new node at start , end or specific position in doubly linkedlist
+  remove a new node at start , end or specific position in doubly linkedlist
+  reverse doubly linked list
+  circular linked list implementation
+  add a new node at start , end or specific position in circular linkedlist
+  remove a new node at start , end or specific position in circular linkedlist
+  reverse circular linked list
+  doubly circular linked list implementation
+  add a new node at start , end or specific position in doubly circular linkedlist
+  remove a new node at start , end or specific position in doubly circular linkedlist
+  reverse doubly circular linked list
+ 
+
+9 stack implementation using array
+infix prefix and postfix expression conversion using stack
+infix to postfix
+prefix to infix conversion
+postfix to prefix conversion
+10 stack in c++ using stl
+
+
+queue implementation using linked list
+circular queue in data structure
+circular queue using array
+queue using stack
+dequeue Double ended queue
+implementation of deque using circular array
+11 queue implementation using c
+12 queue implementation using c++ stl
+13 circular queue c using array
+14 double ended queue (deque ) in c++ using stl
+15 linear search 
+16 binary search
+17 bubble sort
+18 selection sort
+19 insertion sort
+20 quick sort
+21 merge sort
+22 heap tree heap max
+23 heap sort
+24 radix sort bucket sort
+25 shell sort
+26 radix sort
+27 counting sort
+
+tree
+binary tree and its type
+binary tree 
+array representation of binary tree
+binary tree traversals (inorder , preorder and postorder)
+binary search tree
+construct binary tree from preorder
+construct binary tree from postorder
+construct binary tree from preorder
+
+avl tree 
+avl tree insertion
+avl tree traversal
+avl tree deletion
+
+
+red black tree
+red black tree insertion
+deletion
+
+
+binary expression tree =
+expresion tree from postfix
+
+22 binary tree implementation
+23 binary tree level order traversal
+24 binary search tree insertion
+25 search a node in binarysearch tree
+26 node delete from binarysearchtree
+26 heap
+27 building max heap
+28 insert element in max heap
+29 delete element from heap
+30 heap in c++ using stl
+31 hashing 
+32 separete chainning
+33 open addressing
+34 hashing in c++
+35 
+
+
+bfs dfs
