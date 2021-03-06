6fd8da63f36a732179987927f4e5acbbae8725e9
IAMMOIZ <moizmit02@gmail.com>
2021-03-06 12:22:18 +0530

    small update

--- dsContent.c
+++ dsContent.c
@@ -1,98 +1,93 @@
 1 basic of datastructure
 2 array
-
 3 linked list , singlly linked list , doublly linked list , circular doublly linked list
 4 array vs linkList
 5 linked list implementation
 6 add a new node at start , end or specific position in linkedlist
 7 delete a node from start , end or specific position in linkedlist
-find length of linked list 
-8 reverse linkedlist
-5 doubly linked list implementation
-  add a new node at start , end or specific position in doubly linkedlist
-  remove a new node at start , end or specific position in doubly linkedlist
-  reverse doubly linked list
-  circular linked list implementation
-  add a new node at start , end or specific position in circular linkedlist
-  remove a new node at start , end or specific position in circular linkedlist
-  reverse circular linked list
-  doubly circular linked list implementation
-  add a new node at start , end or specific position in doubly circular linkedlist
-  remove a new node at start , end or specific position in doubly circular linkedlist
-  reverse doubly circular linked list
+8 find length of linked list 
+9 reverse linkedlist
+10 doubly linked list implementation
+11 add a new node at start , end or specific position in doubly linkedlist
+12  remove a new node at start , end or specific position in doubly linkedlist
+13  reverse doubly linked list
+14  circular linked list implementation
+15  add a new node at start , end or specific position in circular linkedlist
+16  remove a new node at start , end or specific position in circular linkedlist
+17  reverse circular linked list
+18  doubly circular linked list implementation
+19  add a new node at start , end or specific position in doubly circular linkedlist
+20  remove a new node at start , end or specific position in doubly circular linkedlist
+21  reverse doubly circular linked list
+22 stack implementation using array
+23 infix prefix and postfix expression conversion using stack
+24 infix to postfix
+25 prefix to infix conversion
+26 postfix to prefix conversion
+27 stack in c++ using stl
+28 queue implementation using linked list
+29 circular queue in data structure
+30 circular queue using array
+31 queue using stack
+32 dequeue Double ended queue
+33 implementation of deque using circular array
+34 queue implementation using c
+35 queue implementation using c++ stl
+36 circular queue c using array
+37 double ended queue (deque ) in c++ using stl
+38 linear search 
+39 binary search
+40 bubble sort
+41 selection sort
+42 insertion sort
+43 quick sort
+44 merge sort
+45 heap tree heap max
+46 heap sort
+47 radix sort bucket sort
+48 shell sort
+49 radix sort
+50 counting sort
+
+51 tree
+52 binary tree and its type
+53 binary tree 
+54 array representation of binary tree
+55 binary tree traversals (inorder , preorder and postorder)
+56 binary search tree
+57 construct binary tree from preorder
+58 construct binary tree from postorder
+59 construct binary tree from preorder
+
+60 avl tree 
+61 avl tree insertion
+62 avl tree traversal
+63 avl tree deletion
+
+
+64 red black tree
+65 red black tree insertion
+66 deletion
+
+
+67 binary expression tree =
+68 expresion tree from postfix
+
+69 binary tree implementation
+70 binary tree level order traversal
+71 binary search tree insertion
+72 search a node in binarysearch tree
+73 node delete from binarysearchtree
+74 heap
+75 building max heap
+76 insert element in max heap
+77 delete element from heap
+78 heap in c++ using stl
+79 hashing 
+80 separete chainning
+81 open addressing
+82 hashing in c++
  
 
-9 stack implementation using array
-infix prefix and postfix expression conversion using stack
-infix to postfix
-prefix to infix conversion
-postfix to prefix conversion
-10 stack in c++ using stl
 
-
-queue implementation using linked list
-circular queue in data structure
-circular queue using array
-queue using stack
-dequeue Double ended queue
-implementation of deque using circular array
-11 queue implementation using c
-12 queue implementation using c++ stl
-13 circular queue c using array
-14 double ended queue (deque ) in c++ using stl
-15 linear search 
-16 binary search
-17 bubble sort
-18 selection sort
-19 insertion sort
-20 quick sort
-21 merge sort
-22 heap tree heap max
-23 heap sort
-24 radix sort bucket sort
-25 shell sort
-26 radix sort
-27 counting sort
-
-tree
-binary tree and its type
-binary tree 
-array representation of binary tree
-binary tree traversals (inorder , preorder and postorder)
-binary search tree
-construct binary tree from preorder
-construct binary tree from postorder
-construct binary tree from preorder
-
-avl tree 
-avl tree insertion
-avl tree traversal
-avl tree deletion
-
-
-red black tree
-red black tree insertion
-deletion
-
-
-binary expression tree =
-expresion tree from postfix
-
-22 binary tree implementation
-23 binary tree level order traversal
-24 binary search tree insertion
-25 search a node in binarysearch tree
-26 node delete from binarysearchtree
-26 heap
-27 building max heap
-28 insert element in max heap
-29 delete element from heap
-30 heap in c++ using stl
-31 hashing 
-32 separete chainning
-33 open addressing
-34 hashing in c++
-35 
-
-
-bfs dfs
+83 bfs dfs
