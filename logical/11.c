f1aae1728ffcf9769c759d8c3543b9d1c17b503d
IAMMOIZ <moizmit02@gmail.com>
2021-04-14 10:34:37 +0530

    logical coding 11 to 16

---
+++ logical/11.c
@@ -0,0 +1,11 @@
+//program to accept a number from user and print there square and cube
+#include<stdio.h>
+int main()
+{
+int num;
+printf("Enter a number : ");
+scanf("%d",&num);
+printf("square is %d\n",num*num);
+printf("cube is %d",num*num*num);
+return 0;
+}
